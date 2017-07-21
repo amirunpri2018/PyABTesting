02c2d25b0cf9ec0450da1483a65c8fcc5895a871
Ivan Liu <ivan.liuyanfeng@gmail.com>
2017-07-22 00:17:20 +1000

    oanda test

--- firstStrategy/firstStrategy_PyFolio.py
+++ firstStrategy/firstStrategy_PyFolio.py
@@ -13,18 +13,18 @@ import random
 import pyfolio as pf
 from copy import deepcopy
 
-# import rpy2's package module
-import rpy2.robjects.packages as rpackages
-# import R's utility package
-utils = rpackages.importr('utils')
-# select a mirror for R packages
-utils.chooseCRANmirror(ind=1) # select the first mirror in the list
-# R package names
-packnames = ('PerformanceAnalytics')
-# R vector of strings
-from rpy2.robjects.vectors import StrVector
-# Selectively install what needs to be install.
-utils.install_packages(packnames)
+## import rpy2's package module
+#import rpy2.robjects.packages as rpackages
+## import R's utility package
+#utils = rpackages.importr('utils')
+## select a mirror for R packages
+#utils.chooseCRANmirror(ind=1) # select the first mirror in the list
+## R package names
+#packnames = ('PerformanceAnalytics')
+## R vector of strings
+#from rpy2.robjects.vectors import StrVector
+## Selectively install what needs to be install.
+#utils.install_packages(packnames)
 
 from rpy2.robjects.packages import importr
 pa = importr("PerformanceAnalytics")    # The R package PerformanceAnalytics, containing the R function VaR
